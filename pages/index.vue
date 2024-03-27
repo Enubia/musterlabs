dc6bd34dc14983596acb225b1e147b8faa473fed
Enubia <44695100+Enubia@users.noreply.github.com>
2024-03-27 14:31:22 +0100

    update: refactor code to improve performance and readability

--- pages/index.vue
+++ pages/index.vue
@@ -12,115 +12,134 @@ useSeoMeta({
         </div>
     </header>
     <main class="container">
-        <div class="my-8 flex justify-center">
-            <h1 class="text-3xl text-primary">
-                Your one stop solution for all things tech.
+        <section class="mt-40 lg:px-40">
+            <h1>
+                Hi, I'm Marco.
             </h1>
-        </div>
-        <section class="flex justify-center lg:px-20">
             <p>
-                Hi, my name is Marco. I am a software engineer and I love to build things. I am passionate about
-                technology and I love to share my knowledge with others. I created this website to share my projects
-                and
-                ideas with you. I hope you find it useful and inspiring. Enjoy!
+                I'm a passionate software developer with a strong focus on web development.
+                I love to create and build things that are useful and make a difference.
             </p>
         </section>
-        <section class="my-8 flex justify-center lg:px-20">
-            <div>
-                <h2 class="mb-2 text-2xl text-primary">
-                    Past and current Projects
-                </h2>
-                <div class="flex flex-col items-center gap-5 md:flex-row">
-                    <a
-                        href="https://github.com/enubia/ghost-chat" target="_blank" rel="noopener noreferrer"
-                        class="hover:shadow-lg"
-                    >
-                        <Card>
-                            <CardHeader>
-                                <div class="flex justify-center">
-                                    <img src="/images/ghostchat.png" alt="Ghost Chat" class="size-40">
-                                </div>
-                            </CardHeader>
-                            <CardContent>
-                                <h3 class="font-semibold">
-                                    Ghost Chat
-                                </h3>
-                                <p>
-                                    A standalone, multiplatform Twitch.tv chat as overlay on windowed/windowed
-                                    fullscreen
-                                    applications.
-                                </p>
-                            </CardContent>
-                            <CardFooter />
-                        </Card>
-                    </a>
-                    <a
-                        href="https://donate-gifts.com" target="_blank" rel="noopener noreferrer"
-                        class="hover:shadow-lg"
-                    >
-                        <Card>
-                            <CardHeader>
-                                <div class="flex justify-center">
-                                    <img src="/images/donategifts.png" alt="DonateGifts" class="size-40">
-                                </div>
-                            </CardHeader>
-                            <CardContent>
-                                <h3 class="font-semibold">
-                                    DonateGifts
-                                </h3>
-                                <p>
-                                    A standalone, multiplatform Twitch.tv chat as overlay on windowed/windowed
-                                    fullscreen
-                                    applications.
-                                </p>
-                            </CardContent>
-                            <CardFooter />
-                        </Card>
-                    </a>
+        <section class="my-8 lg:px-40">
+            <h2 class="mb-2 text-2xl text-primary">
+                Technologies I'm familiar with
+            </h2>
+            <div class="mt-3 flex flex-wrap items-center justify-center gap-4">
+                <div class="flex items-center gap-1">
+                    <Icon name="mdi:language-typescript" class="size-10" style="color: #3178c6" />
+                    <span>Typescript</span>
                 </div>
-                <div class="mt-5 flex flex-col items-center gap-5 md:flex-row">
-                    <a
-                        href="https://fitness-elite.eu" target="_blank" rel="noopener noreferrer"
-                        class="hover:shadow-lg"
-                    >
-                        <Card>
-                            <CardHeader>
-                                <div class="flex justify-center">
-                                    <img src="/images/fitness-elite.webp" alt="Fitness Elite" class="w-full">
-                                </div>
-                            </CardHeader>
-                            <CardContent>
-                                <h3 class="font-semibold">
-                                    Fitness Elite
-                                </h3>
-                                <p>
-                                    UNLEASH YOUR FULL POTENTIAL
-                                    WITH THE ALL IN ONE COACHING
-                                    ”ELITE EVOLUTION”
-                                </p>
-                            </CardContent>
-                            <CardFooter />
-                        </Card>
-                    </a>
+                <div class="flex items-center gap-1">
+                    <Icon name="mdi:vuejs" class="size-10" style="color: #42b883" />
+                    <span>Vue.js</span>
+                </div>
+                <div class="flex items-center gap-1">
+                    <Icon name="mdi:react" class="size-10" style="color: #61dafb" />
+                    <span>React</span>
+                </div>
+                <div class="flex items-center gap-1">
+                    <Icon name="mdi:nodejs" class="size-10" style="color: #42b883" />
+                    <span>Node.js</span>
+                </div>
+                <div class="flex items-center gap-1">
+                    <Icon name="simple-icons:pug" class="size-10 text-yellow-900" />
+                    <span>Pug.js</span>
+                </div>
+                <div class="flex items-center gap-1">
+                    <Icon name="simple-icons:mongodb" class="size-10" style="color: #47a248" />
+                    <span>MongoDB</span>
+                </div>
+                <div class="flex items-center gap-1">
+                    <Icon name="simple-icons:postgresql" class="size-10" style="color: #336791" />
+                    <span>PostgreSQL</span>
+                </div>
+                <div class="flex items-center gap-1">
+                    <Icon name="simple-icons:docker" class="size-10" style="color: #2496ed" />
+                    <span>Docker</span>
                 </div>
             </div>
         </section>
+        <section class="my-8 lg:px-40">
+            <h2 class="mb-2 text-2xl text-primary">
+                Projects
+            </h2>
+            <div class="flex flex-col items-center gap-5 md:flex-row">
+                <a
+                    href="https://github.com/enubia/ghost-chat" target="_blank" rel="noopener noreferrer"
+                    class=" md:w-1/2"
+                >
+                    <Card class="border-none bg-background hover:shadow-lg">
+                        <CardHeader />
+                        <CardContent>
+                            <h3 class="font-semibold">
+                                Ghost Chat
+                            </h3>
+                            <p>
+                                A standalone, multi platform Twitch.tv chat as overlay for windowed- and windowed-fullscreen applications.
+                            </p>
+                        </CardContent>
+                    </Card>
+                </a>
+                <a href="https://donate-gifts.com" target="_blank" rel="noopener noreferrer" class="md:w-1/2">
+                    <Card class="border-none bg-background hover:shadow-lg">
+                        <CardHeader />
+                        <CardContent>
+                            <h3 class="font-semibold">
+                                DonateGifts
+                            </h3>
+                            <p>
+                                Holiday gifts and school supplies for children in foster care and emergency shelters.
+                            </p>
+                        </CardContent>
+                    </Card>
+                </a>
+            </div>
+            <div class="mt-5 flex flex-col items-center gap-5 md:flex-row">
+                <a href="https://fitness-elite.eu" target="_blank" rel="noopener noreferrer" class="md:w-1/2">
+                    <Card class="border-none bg-background hover:shadow-lg">
+                        <CardHeader />
+                        <CardContent>
+                            <h3 class="font-semibold">
+                                Fitness Elite
+                            </h3>
+                            <p>
+                                Unleash your full potential
+                                with the all in one coaching
+                                ”ELITE EVOLUTION”
+                            </p>
+                        </CardContent>
+                    </Card>
+                </a>
+            </div>
+        </section>
     </main>
-    <footer>
+    <footer class=" text-muted-foreground">
         <div class="justify-center p-4 text-center md:flex">
             <p>Lets get in touch:</p>
             <a href="mailto:musterlabs@mailbox.org" class="ml-2 text-primary">musterlabs[at]mailbox.org</a>
         </div>
         <div class="mb-4 text-center md:mb-0">
             <p>
-                &copy; {{ new Date().getFullYear() }} Musterlabs. All rights reserved.
+                2024 - PRESENT &copy; Musterlabs
             </p>
-            <a href="https://github.com/enubia" target="_blank" rel="noopener noreferrer">
-                <Icon name="mdi:github" class="size-10 text-secondary hover:text-primary" />
-            </a>
-            <a href="https://www.linkedin.com/in/marco-schuster-37b70a140/" target="_blank" rel="noopener noreferrer">
-                <Icon name="mdi:linkedin" class="size-10 text-secondary hover:text-primary" />
-            </a>
+            <div class="mt-4 flex items-center justify-center gap-3">
+                <div class="underline underline-offset-[12px] hover:cursor-pointer hover:text-primary">
+                    <a href="https://github.com/enubia" target="_blank" rel="noopener noreferrer">
+                        <Icon name="mdi:github" class="size-10" />
+                    </a>
+                    <span>GitHub</span>
+                </div>
+                <div class="underline underline-offset-[12px] hover:cursor-pointer hover:text-primary">
+                    <a
+                        href="https://www.linkedin.com/in/marco-schuster-37b70a140/" target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Icon name="mdi:linkedin" class="size-10" />
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
